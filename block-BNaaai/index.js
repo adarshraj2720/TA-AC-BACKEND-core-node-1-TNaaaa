7cd88ca3dbb710d49ebcfca2f4298d20ad900df7
Adarsh Raj <adarshraj2721@gmail.com>
2022-03-14 23:23:21 +0530

    block-Bnaaai

---
+++ block-BNaaai/index.js
@@ -0,0 +1,19 @@
+var url = require("url");
+const parsedUrl = url.parse(
+  "https://blog.altcampus.io:80/students/register?name=altcampus&gender=male",
+  true
+);
+
+console.log(parsedUrl);
+console.log(parsedUrl.pathname)
+console.log(parsedUrl.query)
+console.log(parsedUrl.host,parsedUrl.protocol)
+
+
+
+var url1 = require("url");
+const parsedUrlgoogle = url1.parse(
+  "https://www.google.com/",
+  true
+);
+console.log(parsedUrlgoogle)
